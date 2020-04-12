b7bfe79f1894952d9ce5bbb0cfe879401ae1c8c2
yuyin ding <you@example.com>
2020-04-12 15:30:07 -0400

    0412

--- src/Plateforme.java
+++ src/Plateforme.java
@@ -1,12 +1,25 @@
+import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.paint.Color;
 
 public abstract class Plateforme extends Entity{
 
-    public Plateforme() {
+    public Plateforme(int i) {
         this.hauteur = 10;
-        this.largeur = Math.random() * 95 - 80; // entre 80 et 175px
-        double bordG = this.largeur / 2;
-        double bordD = HighSeaTower.WIDTH - this.largeur / 2;
-        this.posX = Math.random() * (bordD - bordG) + bordG;
+        this.largeur = Math.random() * 95 + 80; // entre 80 et 175px
+        this.posX = Math.random() * (HighSeaTower.WIDTH - this.largeur / 2);
+        this.posY = HighSeaTower.HEIGHT - 90 - i * 100;
+    }
+
+    public void update(double dt) {
+        super.update(dt);
+    }
+
+    @Override
+    public void draw(GraphicsContext context, double fenetreX, double fenetreY) {
+        double xAffiche = posX - fenetreX;
+        double yAffiche = posY - fenetreY;
+
+        context.setFill(color);
+        context.fillRect(xAffiche, yAffiche, largeur, hauteur);
     }
 }
--- src/PlateformeSolide.java
+++ src/PlateformeSolide.java
@@ -1,6 +1,11 @@
 import javafx.scene.canvas.GraphicsContext;
 
 public class PlateformeSolide extends Plateforme {
+    public PlateformeSolide(int i) {
+        super(i);
+
+    }
+
     public void draw(GraphicsContext context, double fenetreX, double fenetreY) {
 
     }
